ba47d0fe6f1bff6ce0b8f0cb0b694dd4ffca379c
RubenC2 <rubenct1993@gmail.com>
2025-01-22 14:24:56 +0100

    add basic header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+// import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id="header">
+      <section id="header__content">
+        <article className="foto">
+          <img src="/images/RubenCastro.png" alt="Mi Foto" />
+        </article>
+        <article className="presentacion">
+          <h1>Rubén Castro</h1>
+          <p>Desarrollador Web Full Stack</p>
+        </article>
+        <a href="/CVRubenCastroDev.pdf" className="download-cv">Descargar CV</a>
+        <a
+          href="https://www.linkedin.com/in/rubencastrodev/"
+          className="linkedin-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi LinkedIn
+        </a>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
