53733c10a1234d61a2edd5a27db9af4ccf3e73df
RubenC2 <rubenct1993@gmail.com>
2025-02-06 13:18:33 +0100

    addContextOK

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,18 +1,18 @@
 
 
-const Nav = ({ menuOpen, setMenuOpen, darkMode }) => {
+const Nav = ({ menuOpen, setMenuOpen }) => {
   return (
-    <nav className={`nav ${menuOpen ? "nav--open" : ""} ${darkMode ? "dark" : ""}`} role="navigation">
-      <a href="#about" className="nav__link" onClick={() => setMenuOpen(false)}>
+    <nav className={`nav ${menuOpen ? "nav--open" : ""}`} role="navigation">
+      <a href="#about" className="nav_link" onClick={() => setMenuOpen(false)}>
         Sobre mí
       </a>
-      <a href="#projects" className="nav__link" onClick={() => setMenuOpen(false)}>
+      <a href="#projects" className="nav_link" onClick={() => setMenuOpen(false)}>
         Proyectos
       </a>
-      <a href="#skills" className="nav__link" onClick={() => setMenuOpen(false)}>
+      <a href="#skills" className="nav_link" onClick={() => setMenuOpen(false)}>
         Skills
       </a>
-      <a href="#contact" className="nav__link" onClick={() => setMenuOpen(false)}>
+      <a href="#contact" className="nav_link" onClick={() => setMenuOpen(false)}>
         Contacto
       </a>
     </nav>
