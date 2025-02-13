0b13e35165d2cd3414755b8007342ce2e5184479
RubenC2 <rubenct1993@gmail.com>
2025-02-13 13:45:31 +0100

    add about style, add variables sass, add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ThemeContext } from "../../context/ThemeContext";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const { darkMode } = useContext(ThemeContext);
+
+  return (
+    <footer className={`footer ${darkMode ? "footer--dark" : "footer--light"}`}>
+      <p>© {new Date().getFullYear()} - Portfolio Rubén Castro. Todos los derechos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
+
--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../../context/ThemeContext';
 
 const About = () => {
-  return <section id="about">
-    <article>
-    <h2 className="title-about">Sobre mí</h2>
-  <p className="text-about">
-  Con trayectoria en el ámbito educativo y social, he reorientado mi carrera profesional hacia el desarrollo web, 
-  estudiando un bootcamp intensivo e inmersivo full time en The Bridge, impulsado por el deseo de aprender y afrontar nuevos retos. 
-  Siempre en fase de crecimiento técnico, mi entusiasmo y compromiso me permiten avanzar rápidamente en este campo. 
-  Además, cuento con soft skills claves, como la empatía, las habilidades sociales y comunicativas, la escucha activa y 
-  el trabajo en equipo, lo que contribuye a crear un ambiente de trabajo positivo y productivo. 
-  Estoy en búsqueda de nuevas oportunidades para formar parte de proyectos innovadores.
-  </p>
-  </article>
-  </section>;
+  const { darkMode } = useContext(ThemeContext);
+
+  return (
+    <section id="about" className={`about ${darkMode ? 'about--dark' : 'about--light'}`}>
+      <article className="about_content">
+        <h2 className="about_title">Sobre mí</h2>
+        <p className="about_text">
+          Con trayectoria en el ámbito educativo y social, he reorientado mi carrera profesional hacia el desarrollo web, 
+          estudiando un bootcamp intensivo e inmersivo full time en The Bridge, impulsado por el deseo de aprender y afrontar nuevos retos. 
+          Siempre en fase de crecimiento técnico, mi entusiasmo y compromiso me permiten avanzar rápidamente en este campo. 
+          Además, cuento con soft skills claves, como la empatía, las habilidades sociales y comunicativas, la escucha activa y 
+          el trabajo en equipo, lo que contribuye a crear un ambiente de trabajo positivo y productivo. 
+          Estoy en búsqueda de nuevas oportunidades para formar parte de proyectos innovadores.
+        </p>
+      </article>
+    </section>
+  );
 };
 
 export default About;
