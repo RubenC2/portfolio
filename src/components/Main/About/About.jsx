12f90abf81e60a68079304f17ad0fab5568cacda
RubenC2 <rubenct1993@gmail.com>
2025-02-13 16:54:03 +0100

    add init projects, and about style

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <h2 className="about_title">Sobre mí</h2>
         <p className="about_text">
           Con trayectoria en el ámbito educativo y social, he reorientado mi carrera profesional hacia el desarrollo web, 
-          estudiando un bootcamp intensivo e inmersivo full time en The Bridge, impulsado por el deseo de aprender y afrontar nuevos retos. 
+          estudiando el bootcamp intensivo e inmersivo full time de Desarrollo Web Full Stack, en The Bridge, impulsado por el deseo de aprender y afrontar nuevos retos. 
           Siempre en fase de crecimiento técnico, mi entusiasmo y compromiso me permiten avanzar rápidamente en este campo. 
           Además, cuento con soft skills claves, como la empatía, las habilidades sociales y comunicativas, la escucha activa y 
           el trabajo en equipo, lo que contribuye a crear un ambiente de trabajo positivo y productivo. 
